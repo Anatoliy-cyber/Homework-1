b19bb6f69a909c52f53958d603b5bcd137b4a7a3
Anatoliy-cyber <tperepelicyn@mail.ru>
2025-03-06 23:33:33 +0300

    Homework1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача №1:переменной dog значение 8.0;
-        //переменной cat значение 3.6;
-        //переменной paper значение 763789.
+        //Задача №1
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
